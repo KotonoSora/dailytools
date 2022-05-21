4581a32afabba301924fd110cd880a625ceb05a1
Nguyễn Duy Thắng <duythang.2912@kotonosora.com>
2022-05-22 01:57:00 +0700

    add skeleton inventory app

--- app/src/main/java/com/kotonosora/dailytools/InventoryActivity.kt
+++ app/src/main/java/com/kotonosora/dailytools/InventoryActivity.kt
@@ -2,10 +2,25 @@ package com.kotonosora.dailytools
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 
-class InventoryActivity : AppCompatActivity() {
+class InventoryActivity : AppCompatActivity(R.layout.activity_inventory) {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inventory)
+
+        // Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.inventory_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/kotonosora/dailytools/ui/inventory/ItemListFragment.kt
@@ -0,0 +1,36 @@
+package com.kotonosora.dailytools.ui.inventory
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kotonosora.dailytools.R
+import com.kotonosora.dailytools.databinding.FragmentItemListBinding
+
+class ItemListFragment : Fragment() {
+    private var _binding: FragmentItemListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentItemListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
+        binding.floatingActionButton.setOnClickListener {
+            val action = ItemListFragmentDirections.actionItemListFragmentToAddItemFragment(
+                getString(R.string.add_fragment_title)
+            )
+            this.findNavController().navigate(action)
+        }
+    }
+}
